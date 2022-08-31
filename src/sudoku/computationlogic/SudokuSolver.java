34fd60b41ac106b0575ad1525c96f5cbedab8e14
Caden Murphy <murphycadenr@gmail.com>
2022-08-30 20:50:33 -0500

    First Commit

---
+++ src/sudoku/computationlogic/SudokuSolver.java
@@ -0,0 +1,12 @@
+package sudoku.computationlogic;
+
+public class SudokuSolver {
+	public static boolean puzzleIsSolveable(int [][] puzzle)
+	{
+		Coordinates[] emptyCells = typeWriterEnumerate(puzzle);
+	}
+
+	private static Coordinates[] typeWriterEnumerate(int[][] puzzle) {
+		return null;
+	}
+}
---
+++ src/sudoku/problemdomain/main.java
@@ -0,0 +1,8 @@
+package sudoku.problemdomain;
+
+public class main {
+	public static void main(String[] args)
+	{
+		SudokuApplication.main(new String[]{});
+	}
+}
